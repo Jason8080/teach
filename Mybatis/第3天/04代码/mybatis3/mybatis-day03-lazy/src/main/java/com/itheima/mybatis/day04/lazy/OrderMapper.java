232528f5a8755798b633a75675c4a917a897a85f
Jason <569284276@qq.com>
2019-04-28 15:37:52 +0800

    mybatis-day03-all: init.

---
+++ Mybatis/第3天/04代码/mybatis3/mybatis-day03-lazy/src/main/java/com/itheima/mybatis/day04/lazy/OrderMapper.java
@@ -0,0 +1,18 @@
+package com.itheima.mybatis.day04.lazy;
+
+import java.util.List;
+
+/**
+ * 操作类.
+ * <p>
+ *      用于操作数据库
+ * </p>
+ */
+public interface OrderMapper {
+    /**
+     * 查询所有订单.
+     *
+     * @return the list
+     */
+    List<Order> findO2O();
+}
---
+++ Mybatis/第3天/04代码/mybatis3/mybatis-day03-lazy/src/test/java/LazyTests.java
@@ -0,0 +1,12 @@
+/**
+ * 懒加载测试入口.
+ *
+ * @author ：Jason.lee
+ * @version : 1.0
+ * @date ：2019/4/18 11:59
+ * @description : LazyTests
+ * @modified : -
+ */
+public class LazyTests {
+
+}
