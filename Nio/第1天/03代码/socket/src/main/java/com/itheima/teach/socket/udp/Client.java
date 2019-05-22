3ecf2799ae992a321ad5c3d8aefbd6495e596bd5
Jason.lee <xiaoku13141@163.com>
2019-05-22 09:44:33 +0800

    Nio-网授2天

--- Nio/第1天/03代码/socket/src/main/java/com/itheima/teach/socket/udp/Client.java
+++ Nio/第1天/03代码/socket/src/main/java/com/itheima/teach/socket/udp/Client.java
@@ -1,10 +1,10 @@
 package com.itheima.teach.socket.udp;
 
 import com.itheima.teach.socket.Address;
+import com.itheima.teach.socket.Console;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.SocketException;
 
 /**
  * Client1.
@@ -20,24 +20,34 @@ public class Client {
     public static void main(String[] args) throws Exception {
         //1. 创建客户端
         DatagramSocket socket = new DatagramSocket();
-        //2. 准备数据
-        byte[] bytes = "客户端: 你好".getBytes();
-        //3. 创建集装箱 并 封装数据
-        DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
-        //4. 设置目标地址
-        packet.setSocketAddress(Address.loc);
-        //5. 发送集装箱
-        socket.send(packet);
+
+        for (int i = 0; true; i++) {
+            //      获取控制台1行输入
+            String content = Console.reader.readLine();
+            if("stop".equals(content)){
+                break;
+            }
+
+            //2. 准备数据
+            byte[] bytes = "客户端: ".concat(content).getBytes();
+            //3. 创建集装箱 并 封装数据
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
+            //4. 设置目标地址
+            packet.setSocketAddress(Address.loc);
+            //5. 发送集装箱
+            socket.send(packet);
 
 
-        //1. 准备新的集装箱
-        byte[] by = new byte[1024];
-        DatagramPacket pack = new DatagramPacket(by, by.length);
-        //2. 接收数据并封装到集装箱
-        socket.receive(pack);
-        //3. 打印数据
-        String content = new String(by, 0, pack.getLength());
-        System.out.println(content);
+            //1. 准备新的集装箱
+            byte[] bs = new byte[1024];
+            DatagramPacket pack = new DatagramPacket(bs, bs.length);
+            //2. 接收数据并封装到集装箱
+            socket.receive(pack);
+            //3. 打印数据
+            String res = new String(bs, 0, pack.getLength());
+            System.out.println(res);
+
+        }
 
         //关闭资源
         socket.close();
--- Nio/第1天/03代码/socket/src/main/java/com/itheima/teach/socket/udp/Server.java
+++ Nio/第1天/03代码/socket/src/main/java/com/itheima/teach/socket/udp/Server.java
@@ -1,11 +1,10 @@
 package com.itheima.teach.socket.udp;
 
 import com.itheima.teach.socket.Address;
+import com.itheima.teach.socket.Console;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
 
 /**
  * Server.
@@ -21,24 +20,35 @@ public class Server {
     public static void main(String[] args) throws Exception {
         //1. 创建客户端 并 绑定主机地址
         DatagramSocket socket = new DatagramSocket(Address.loc);
-        byte[] bytes = new byte[1024];
-        //2. 创建空的集装箱
-        DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
-        //3. 接收数据
-        socket.receive(packet);
-        //4. 读取集装箱中的数据
-        String content = new String(bytes, 0, packet.getLength());
-        //5. 打印数据
-        System.out.println(content);
+
+        for (int i = 0; true; i++) {
+
+            byte[] bytes = new byte[1024];
+            //2. 创建空的集装箱
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length);
+            //3. 接收数据
+            socket.receive(packet);
+            //4. 读取集装箱中的数据
+            String req = new String(bytes, 0, packet.getLength());
+            //5. 打印数据
+            System.out.println(req);
 
 
-        // ==================   回复  ====================
-        //1. 准备数据
-        byte[] by = "服务端: 收到了".getBytes();
-        //2. 创建新的集装箱 并 封装数据 设置 目标地址
-        DatagramPacket pack = new DatagramPacket(by, by.length, packet.getAddress(), packet.getPort());
-        //3. 发送集装箱
-        socket.send(pack);
+            // ==================   回复  ====================
+            //      获取控制台1行输入
+            String content = Console.reader.readLine();
+            if("stop".equals(content)){
+                break;
+            }
+
+            //1. 准备数据
+            byte[] bs = "服务端: ".concat(content).getBytes();
+            //2. 创建新的集装箱 并 封装数据 设置 目标地址
+            DatagramPacket pack = new DatagramPacket(bs, bs.length, packet.getAddress(), packet.getPort());
+            //3. 发送集装箱
+            socket.send(pack);
+
+        }
 
         // 关闭资源
         socket.close();
